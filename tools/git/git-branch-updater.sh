a603ad3b47b711cca66b97715751696be194c769
hclaro <hclaro@transportationamerica.com>
2024-06-19 23:29:37 -0500

    fix: updated script new menu option git-branch-updater.sh

--- tools/git/git-branch-updater.sh
+++ tools/git/git-branch-updater.sh
@@ -7,27 +7,35 @@ RIGHT_ARROW=$'\xe2\x9e\xa1'  # ➡
 HEAVY_CHECK_MARK=$'\xe2\x9c\x94'  # ✔
 OPEN_FILE_FOLDER=$'\xf0\x9f\x93\x82'  # 📂
 
+# Definir variables para las ramas
+DEV="devel"
+BETA="beta"
+MASTER="master"
+
+# Variable para omitir preguntas
+OMITIR_PREGUNTAS=false
+
 preguntar_y_evaluar() {
   local pregunta="$1"
   local mensaje_continuar="$2"
   local mensaje_salir="$3"
 
   echo -e "\n_____________________________________________________________________________________"
-  echo -e "$pregunta"
-  read -p "¿Desea continuar? (presione Enter para continuar, cualquier otra tecla para salir): " respuesta
+      echo -e "$pregunta"
+      read -p "¿Desea continuar? (presione Enter para continuar, cualquier otra tecla para salir): " respuesta
 
-  if [ -z "$respuesta" ]; then
-      echo -e "$mensaje_continuar"
-      echo -e "_____________________________________________________________________________________"
-      # Agregar aquí las operaciones adicionales si el usuario elige continuar
-  else
-      echo -e "$mensaje_salir"
-      echo -e "_____________________________________________________________________________________"
-      exit 0
+      if [ -z "$respuesta" ]; then
+        echo -e "$mensaje_continuar"
+        echo -e "_____________________________________________________________________________________"
+      else
+        echo -e "$mensaje_salir"
+        echo -e "_____________________________________________________________________________________"
+        exit 0
   fi
 }
 
 updated-branch-git-repository() {
+  echo -e "\n########################################################################################"
 
   local local_branch="$1"
   local origin_merge_local="$2"
@@ -36,66 +44,114 @@ updated-branch-git-repository() {
   echo -e "$HEAVY_CHECK_MARK for merge (remote): $origin_merge_local"
   echo -e "******************** $origin_merge_local $RIGHT_ARROW $local_branch ********************"
 
-  preguntar_y_evaluar "Confirmar Ramas" "Continuing with the script...\n" "Cancellation... Exit del script..."
+  preguntar_y_evaluar "Confirmar Ramas" "Continuando con el script...\n" "Cancelación... Saliendo del script..."
 
   if git rev-parse --verify $origin_merge_local > /dev/null 2>&1; then
-      git checkout $origin_merge_local
-      git pull origin $origin_merge_local
-      echo -e "$CHECK_ICON  La rama origin fue actualizada en local '$origin_merge_local'."
+    git checkout $origin_merge_local
+    git pull origin $origin_merge_local
+    echo -e "$CHECK_ICON  La rama origin fue actualizada en local '$origin_merge_local'."
   fi
 
   # Validar si la rama local existe, de lo contrario, traerla del origin
   if git rev-parse --verify $local_branch > /dev/null 2>&1; then
-      echo -e "$CHECK_ICON  La rama local '$local_branch' existe."
+    echo -e "$CHECK_ICON  La rama local '$local_branch' existe."
   else
-      echo -e "$CROSS_ICON  La rama local '$local_branch' no existe. Se creará a partir de 'origin/$local_branch'."
+    echo -e "$CROSS_ICON  La rama local '$local_branch' no existe. Se creará a partir de 'origin/$local_branch'."
 
-      if git rev-parse --verify "origin/$local_branch" > /dev/null 2>&1; then
-          git checkout -b $local_branch origin/$local_branch
-          echo -e "$CHECK_ICON  Rama local '$local_branch' creada exitosamente."
-      else
-          echo -e "$CROSS_ICON  La rama remota 'origin/$local_branch' no existe. No se puede crear la rama local."
-          exit 0
-      fi
+    if git rev-parse --verify "origin/$local_branch" > /dev/null 2>&1; then
+      git checkout -b $local_branch origin/$local_branch
+      echo -e "$CHECK_ICON  Rama local '$local_branch' creada exitosamente."
+    else
+      echo -e "$CROSS_ICON  La rama remota 'origin/$local_branch' no existe. No se puede crear la rama local."
+      exit 0
+    fi
   fi
 
-
   # Realizar las operaciones deseadas con las ramas
-
-  preguntar_y_evaluar "$RIGHT_ARROW git checkout $local_branch" "Continuing with the script...\n" "Cancellation... Exit del script..."
-  echo -e "$pregunta"
+  if [ "$OMITIR_PREGUNTAS" != true ]; then
+    preguntar_y_evaluar "$RIGHT_ARROW git checkout $local_branch" "Continuando con el script...\n" "Cancelación... Saliendo del script..."
+    echo -e "$pregunta"
+  else
+    echo -e "\n_____________________________________________________________________________________"
+  fi
   git checkout $local_branch
 
-  preguntar_y_evaluar "$RIGHT_ARROW git pull origin $local_branch" "Continuing with the script...\n" "Cancellation... Exit del script..."
-  echo -e "$pregunta"
+  if [ "$OMITIR_PREGUNTAS" != true ]; then
+    preguntar_y_evaluar "$RIGHT_ARROW git pull origin $local_branch" "Continuando con el script...\n" "Cancelación... Saliendo del script..."
+    echo -e "$pregunta"
+  else
+      echo -e "\n_____________________________________________________________________________________"
+  fi
   git pull origin $local_branch
 
-  preguntar_y_evaluar "$RIGHT_ARROW git merge origin/$origin_merge_local" "Continuing with the script...\n" "Cancellation... Exit del script..."
-  echo -e "$pregunta"
+  if [ "$OMITIR_PREGUNTAS" != true ]; then
+    preguntar_y_evaluar "$RIGHT_ARROW git merge origin/$origin_merge_local" "Continuando con el script...\n" "Cancelación... Saliendo del script..."
+    echo -e "$pregunta"
+  else
+      echo -e "\n_____________________________________________________________________________________"
+  fi
   git merge origin/$origin_merge_local
 
-
-  preguntar_y_evaluar "$RIGHT_ARROW ************** git push origin $local_branch **************" "Continuing with the script...\n" "Cancellation... Exit del script..."
+  preguntar_y_evaluar "$RIGHT_ARROW ************** git push origin $local_branch **************" "Continuando con el script...\n" "Cancelación... Saliendo del script..."
   echo -e "$pregunta"
   git push origin $local_branch
 
   # Fin del script
   echo -e "\n ${CHECK_ICON}  Operaciones completadas."
-
+  echo -e "\n_____________________________________________________________________________________\n"
 }
 
+mostrar_menu() {
+  echo "Seleccione una opción:"
+  echo "1) Actualización de $DEV"
+  echo "2) Actualización de $BETA"
+  echo "3) Actualización de $MASTER"
+  echo "4) Manual"
+  read -p "Ingrese el número de la opción: " opcion
+}
 
+preguntar_omitir_preguntas() {
+  read -p "¿Desea omitir las preguntas de confirmación? (s/n): " respuesta_omitir
+  if [[ "$respuesta_omitir" =~ ^[sS]$ ]]; then
+    OMITIR_PREGUNTAS=true
+    echo "Omitir preguntas ha sido activado."
+  else
+    OMITIR_PREGUNTAS=false
+    echo "Omitir preguntas ha sido desactivado."
+  fi
+}
 
 # Mostrar mensaje de inicio
+echo -e "\n########################################################################################"
 echo -e "\n${OPEN_FILE_FOLDER} Updated Remote Project"
+echo -e "======>${CHECK_ICON}  Check Branches"
 
-# Mostrar las ramas ingresadas
-echo -e "\n____________________________________"
-echo -e "${CHECK_ICON}  Check Branches"
-# Solicitar la rama local o remota a actualizar
-  read -p "$RIGHT_ARROW Branch to Upgrade (local): " a
+# Mostrar el menú
+mostrar_menu
 
-  # Solicitar la rama para el merge
-  read -p "$RIGHT_ARROW Branch for merge (remote): " b
-
-updated-branch-git-repository $a $b
+# Procesar la opción seleccionada
+case $opcion in
+  1)
+    preguntar_omitir_preguntas
+    updated-branch-git-repository $DEV $BETA
+    updated-branch-git-repository $DEV $MASTER
+    ;;
+  2)
+    preguntar_omitir_preguntas
+    updated-branch-git-repository $BETA $DEV
+    ;;
+  3)
+    preguntar_omitir_preguntas
+    updated-branch-git-repository $MASTER $BETA
+    ;;
+  4)
+    preguntar_omitir_preguntas
+    read -p "$RIGHT_ARROW Branch to Upgrade (local): " A
+    read -p "$RIGHT_ARROW Branch for merge (remote): " B
+    updated-branch-git-repository $A $B
+    ;;
+  *)
+    echo "Opción no válida. Saliendo..."
+    exit 1
+    ;;
+esac
